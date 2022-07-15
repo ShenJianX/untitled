198c0adc6a195629ec4e046281bc61aef5bb22de
Sjxin <Sjxin@atguigu.com>
2022-07-15 18:27:24 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/test.java
+++ src/main/java/com/atguigu/test.java
@@ -3,5 +3,8 @@ package com.atguigu;
 public class test {
     public static void main(String[] args) {
         System.out.println("11111111");
+        System.out.println("second upda");
+        System.out.println("ssssssssss33");
+        System.out.println("4444444444");
     }
 }
